51b3d37989c0c9e24c3e9315ddc0fc0506e4dafa
diskit <diskit.1980@gmail.com>
2025-11-07 21:56:22 +0900

    add generic col

--- src/attribute-source.ts
+++ src/attribute-source.ts
@@ -10,16 +10,30 @@ export class AttributeSourceExampleTable extends LitElement {
         }
     `;
 
-    json = [
+    name = [
         { id: 1, value: "山田太郎" },
         { id: 2, value: "鈴木花子" },
         { id: 3, value: "佐藤次郎" }
     ]
 
+    address = [
+        { id: 1, value: "東京都新宿区1-1-1" },
+        { id: 2, value: "大阪府大阪市北区2-2-2" },
+        { id: 3, value: "愛知県名古屋市中区3-3-3" }
+    ]
+
+    phone = [
+        { id: 1, value: "090-1234-5678" },
+        { id: 2, value: "080-2345-6789" },
+        { id: 3, value: "070-3456-7890" }
+    ]
+
     render() {
         return html`
-            <list-component .keys="${this.json.map(v => v.id.toString())}">
-                <generic-cell slot="cells" data-source=${JSON.stringify(this.json)} title="氏名"></generic-cell>
+            <list-component .keys="${this.name.map(v => v.id.toString())}">
+                <generic-cell slot="cells" data-source=${JSON.stringify(this.name)} title="氏名"></generic-cell>
+                <generic-cell slot="cells" data-source=${JSON.stringify(this.address)} title="住所"></generic-cell>
+                <generic-cell slot="cells" data-source=${JSON.stringify(this.phone)} title="電話番号"></generic-cell>
             </list-component>`;
     }
 }
